e8c83c8f68f5659169cf0f422f5df5ec10fd5c19
jared <bennett.jaredn@gmail.com>
2019-08-20 22:01:19 -0700

    url working for single post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post 
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
